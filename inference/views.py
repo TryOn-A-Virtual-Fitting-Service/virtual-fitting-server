131c7ac6df3fdc2bb01d85112f4464704e5c2b71
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-09-27 11:54:20 +0900

    image added

--- inference/views.py
+++ inference/views.py
@@ -1,14 +1,26 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+import base64
 
 def generate(request):
-    if request.method == "GET":
-        return JsonResponse({
-            'message': 'Inference Endpoint',
-            'data': "This is the Inference Endpoint",
-        }, status=200)
+    if request.method == "POST":
+        # Assuming 'image.jpg' is in the same directory as this script
+        image_path = '/d:/Capstone/virtual-fitting-server/inference/image.jpg'
+        try:
+            with open(image_path, "rb") as image_file:
+                encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+            return JsonResponse({
+                'message': 'Inference Endpoint',
+                'data': "This is the Inference Endpoint",
+                'image': encoded_string
+            }, status=200)
+        except FileNotFoundError:
+            return JsonResponse({
+                'message': 'File not found',
+                'data': "image.jpg not found",
+            }, status=404)
     else:
         return JsonResponse({
-            'message': 'Only GET is allowed',
-            'data': "Only GET is allowed",
+            'message': 'Only POST is allowed',
+            'data': "Only POST is allowed",
         }, status=405)
