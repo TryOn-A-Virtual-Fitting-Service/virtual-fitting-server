ebea10055996ceeebc00a52f75f937b5c545e9ad
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-09-28 20:18:29 +0900

    dir to curr

--- inference/views.py
+++ inference/views.py
@@ -2,12 +2,13 @@ from django.http import JsonResponse
 from django.shortcuts import render
 import base64
 from django.views.decorators.csrf import csrf_exempt
+import os
 
 @csrf_exempt
 def generate(request):
     if request.method == "POST":
-        # Assuming 'image.jpg' is in the same directory as this script
-        image_path = '/d:/Capstone/virtual-fitting-server/inference/image.jpg'
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        image_path = os.path.join(current_dir, 'image.jpg')
         try:
             with open(image_path, "rb") as image_file:
                 encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
