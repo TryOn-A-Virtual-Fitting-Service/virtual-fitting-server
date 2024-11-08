b18727ef1feb91856d382c8f721bd59733ee5551
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-11-08 11:36:02 +0900

    Update views.py

--- inference/views.py
+++ inference/views.py
@@ -68,8 +68,18 @@ def generate(request):
         #     'sample': 1
         # })
         # os.chdir(original_dir)
-        command = f"source {venv_activate_path} && cd {ootd_working_dir} && python {run_ootd_path} --model_path {model_path} --cloth_path {clothing_path} --scale 1.0 --sample 1"
-        subprocess.run(command, shell=True, check=True, cwd=ootd_working_dir)
+        command = [
+            "source", venv_activate_path,
+            "&&",
+            "cd", ootd_working_dir,
+            "&&",
+            "python", run_ootd_path,
+            "--model_path", model_path,
+            "--cloth_path", clothing_path,
+            "--scale", "1.0",
+            "--sample", "1"
+        ]
+        subprocess.run(" ".join(command), shell=True, check=True, cwd=ootd_working_dir)
     except subprocess.CalledProcessError as e:
         return JsonResponse({
             'message': 'Error running OOTD script',
