9775cd0c1e877db414794711453cc91155d6cded
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-11-08 10:22:59 +0900

    refactoring + dir issue

--- inference/views.py
+++ inference/views.py
@@ -7,82 +7,88 @@ from datetime import datetime
 
 @csrf_exempt
 def generate(request):
-    if request.method == "POST":
-        if 'clothing' not in request.FILES or 'model' not in request.FILES:
-            return JsonResponse({
-                'message': 'Missing files',
-                'data': "Both clothing and model files are required",
-            }, status=400)
-        
-        clothing = request.FILES['clothing']
-        model = request.FILES['model']
-        
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        root_dir = os.path.dirname(current_dir)  # Set root_dir to 'virtual-fitting-server'
-        
-        temp_storage_dir = os.path.join(root_dir, 'temp_storage')
-        clothing_path = os.path.join(temp_storage_dir, 'clothing.jpg')
-        model_path = os.path.join(temp_storage_dir, 'model.jpg')
-        output_path = os.path.join(root_dir, 'OOTDiffusion', 'run', 'images_output', 'out_hd_0.png')
-        fitting_results_dir = os.path.join(root_dir, 'fitting_results')
-        run_ootd_path = os.path.join(root_dir, 'OOTDiffusion', 'run', 'run_ootd.py')
-        
-        if not os.path.exists(fitting_results_dir):
-            try:
-                os.makedirs(fitting_results_dir)
-            except OSError as e:
-                return JsonResponse({
-                    'message': 'Error creating fitting results directory',
-                    'data': str(e),
-                }, status=500)
-        
-        try:
-            # 업로드된 옷과 모델 이미지를 저장
-            with open(clothing_path, 'wb+') as destination:
-                for chunk in clothing.chunks():
-                    destination.write(chunk)
-            with open(model_path, 'wb+') as destination:
-                for chunk in model.chunks():
-                    destination.write(chunk)
-        except Exception as e:
-            return JsonResponse({
-                'message': 'Error saving files',
-                'data': str(e),
-            }, status=500)
-        
-        try:
-            runpy.run_path(run_ootd_path, run_name='__main__', init_globals={
-                'model_path': model_path,
-                'cloth_path': clothing_path,
-                'scale': 1.0,
-                'sample': 1
-            })
-        except Exception as e:
-            return JsonResponse({
-                'message': 'Error running OOTD script',
-                'data': str(e),
-            }, status=500)
-        
-        try:
-            timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-            result_filename = f'result_{timestamp}.png'
-            result_path = os.path.join(fitting_results_dir, result_filename)
-            shutil.move(output_path, result_path)
-        except Exception as e:
-            return JsonResponse({
-                'message': 'Error moving result file',
-                'data': str(e),
-            }, status=500)
-        
-        return JsonResponse({
-            'message': 'Inference successful',
-            'data': result_filename,
-        }, status=200)
-    else:
+    if request.method != "POST":
         return JsonResponse({
             'message': 'Only POST is allowed',
             'data': "Only POST is allowed",
         }, status=405)
+    
+    if 'clothing' not in request.FILES or 'model' not in request.FILES:
+        return JsonResponse({
+            'message': 'Missing files',
+            'data': "Both clothing and model files are required",
+        }, status=400)
+    
+    clothing = request.FILES['clothing']
+    model = request.FILES['model']
+    
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.dirname(current_dir)  # Set root_dir to 'virtual-fitting-server'
+    
+    temp_storage_dir = os.path.join(root_dir, 'temp_storage')
+    clothing_path = os.path.join(temp_storage_dir, 'clothing.jpg')
+    model_path = os.path.join(temp_storage_dir, 'model.jpg')
+    output_path = os.path.join(root_dir, 'OOTDiffusion', 'run', 'images_output', 'out_hd_0.png')
+    fitting_results_dir = os.path.join(root_dir, 'fitting_results')
+    run_ootd_path = os.path.join(root_dir, 'OOTDiffusion', 'run', 'run_ootd.py')
+    ootd_working_dir = os.path.join(root_dir, 'OOTDiffusion', 'run')
+    
+    if not os.path.exists(fitting_results_dir):
+        try:
+            os.makedirs(fitting_results_dir)
+        except OSError as e:
+            return JsonResponse({
+                'message': 'Error creating fitting results directory',
+                'data': str(e),
+            }, status=500)
+    
+    try:
+        # 업로드된 옷과 모델 이미지를 저장
+        with open(clothing_path, 'wb+') as destination:
+            for chunk in clothing.chunks():
+                destination.write(chunk)
+        with open(model_path, 'wb+') as destination:
+            for chunk in model.chunks():
+                destination.write(chunk)
+    except Exception as e:
+        return JsonResponse({
+            'message': 'Error saving files',
+            'data': str(e),
+        }, status=500)
+    original_dir = os.getcwd()
+    try:
+        # OOTDiffusion 인퍼런스 실행
+        os.chdir(ootd_working_dir)
+        runpy.run_path(run_ootd_path, run_name='__main__', init_globals={
+            'model_path': model_path,
+            'cloth_path': clothing_path,
+            'scale': 1.0,
+            'sample': 1
+        })
+        os.chdir(original_dir)
+    except Exception as e:
+        os.chdir(original_dir)
+        return JsonResponse({
+            'message': 'Error running OOTD script',
+            'data': str(e),
+        }, status=500)
+    
+    try:
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        result_filename = f'result_{timestamp}.png'
+        result_path = os.path.join(fitting_results_dir, result_filename)
+        shutil.move(output_path, result_path)
+    except Exception as e:
+        return JsonResponse({
+            'message': 'Error moving result file',
+            'data': str(e),
+        }, status=500)
+    
+    return JsonResponse({
+        'message': 'Inference successful',
+        'data': result_filename,
+    }, status=200)
+
     
 
 @csrf_exempt
