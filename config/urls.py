42f0546a59cbae4679a4097ab4224f9c2e27039e
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-11-12 12:10:16 +0900

    static provider

--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-import inference.urls
+from django.conf.urls.static import static
 from config.views import *
+import inference.urls
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inference/', include(inference.urls)),
     path('', landing_page, name='landing_page'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'results'))
