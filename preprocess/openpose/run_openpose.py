8ca9805e0584819251bdfdc06a354f829f9ba1b0
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-12-10 00:49:02 +0900

    Update run_openpose.py

--- preprocess/openpose/run_openpose.py
+++ preprocess/openpose/run_openpose.py
@@ -36,54 +36,44 @@ class OpenPose:
         torch.cuda.set_device(self.gpu_id)
         if isinstance(input_image, Image.Image):
             input_image = np.asarray(input_image)
-        elif isinstance(input_image, str):
+        elif type(input_image) == str:
             input_image = np.asarray(Image.open(input_image))
         else:
-            raise ValueError("Input must be a PIL Image or a file path string.")
-
-        # Image preprocessing steps remain outside the AMP context
-        input_image = HWC3(input_image)
-        input_image = resize_image(input_image, resolution)
-        H, W, C = input_image.shape
-        assert (H == 512 and W == 384), 'Incorrect input image shape'
-
+            raise ValueError
         with torch.no_grad():
-            # Start AMP autocast context only for PyTorch operations
+            # AMP 적용
             with torch.cuda.amp.autocast():
+                input_image = HWC3(input_image)
+                input_image = resize_image(input_image, resolution)
+                H, W, C = input_image.shape
+                assert (H == 512 and W == 384), 'Incorrect input image shape'
                 pose, detected_map = self.preprocessor(input_image, hand_and_face=False)
 
-        candidate = pose['bodies']['candidate']
-        subset = pose['bodies']['subset'][0][:18]
-        for i in range(18):
-            if subset[i] == -1:
-                candidate.insert(i, [0, 0])
-                for j in range(i, 18):
-                    if subset[j] != -1:
-                        subset[j] += 1
-            elif subset[i] != i:
-                candidate.pop(i)
-                for j in range(i, 18):
-                    if subset[j] != -1:
-                        subset[j] -= 1
+                candidate = pose['bodies']['candidate']
+                subset = pose['bodies']['subset'][0][:18]
+                for i in range(18):
+                    if subset[i] == -1:
+                        candidate.insert(i, [0, 0])
+                        for j in range(i, 18):
+                            if(subset[j]) != -1:
+                                subset[j] += 1
+                    elif subset[i] != i:
+                        candidate.pop(i)
+                        for j in range(i, 18):
+                            if(subset[j]) != -1:
+                                subset[j] -= 1
 
-        candidate = candidate[:18]
+                candidate = candidate[:18]
 
-        for i in range(18):
-            candidate[i][0] *= 384
-            candidate[i][1] *= 512
+                for i in range(18):
+                    candidate[i][0] *= 384
+                    candidate[i][1] *= 512
 
-        keypoints = {"pose_keypoints_2d": candidate}
-        # with open("/home/aigc/ProjectVTON/OpenPose/keypoints/keypoints.json", "w") as f:
-        #     json.dump(keypoints, f)
-        #
-        # # print(candidate)
-        # output_image = cv2.resize(cv2.cvtColor(detected_map, cv2.COLOR_BGR2RGB), (768, 1024))
-        # cv2.imwrite('/home/aigc/ProjectVTON/OpenPose/keypoints/out_pose.jpg', output_image)
+                keypoints = {"pose_keypoints_2d": candidate}
 
         return keypoints
 
 
 if __name__ == '__main__':
-    # Initialize the OpenPose model with the desired GPU ID, e.g., 0
-    model = OpenPose(gpu_id=0)
+    model = OpenPose(0)
     model('./images/bad_model.jpg')
