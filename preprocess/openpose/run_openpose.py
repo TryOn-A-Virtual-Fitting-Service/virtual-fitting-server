7feacaec6be365079decf78c6291f37a40ecc75d
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-12-10 00:53:38 +0900

    Update run_openpose.py

--- preprocess/openpose/run_openpose.py
+++ preprocess/openpose/run_openpose.py
@@ -41,39 +41,44 @@ class OpenPose:
         else:
             raise ValueError
         with torch.no_grad():
-            # AMP 적용
-            with torch.cuda.amp.autocast():
-                input_image = HWC3(input_image)
-                input_image = resize_image(input_image, resolution)
-                H, W, C = input_image.shape
-                assert (H == 512 and W == 384), 'Incorrect input image shape'
-                pose, detected_map = self.preprocessor(input_image, hand_and_face=False)
+            input_image = HWC3(input_image)
+            input_image = resize_image(input_image, resolution)
+            H, W, C = input_image.shape
+            assert (H == 512 and W == 384), 'Incorrect input image shape'
+            pose, detected_map = self.preprocessor(input_image, hand_and_face=False)
 
-                candidate = pose['bodies']['candidate']
-                subset = pose['bodies']['subset'][0][:18]
-                for i in range(18):
-                    if subset[i] == -1:
-                        candidate.insert(i, [0, 0])
-                        for j in range(i, 18):
-                            if(subset[j]) != -1:
-                                subset[j] += 1
-                    elif subset[i] != i:
-                        candidate.pop(i)
-                        for j in range(i, 18):
-                            if(subset[j]) != -1:
-                                subset[j] -= 1
+            candidate = pose['bodies']['candidate']
+            subset = pose['bodies']['subset'][0][:18]
+            for i in range(18):
+                if subset[i] == -1:
+                    candidate.insert(i, [0, 0])
+                    for j in range(i, 18):
+                        if(subset[j]) != -1:
+                            subset[j] += 1
+                elif subset[i] != i:
+                    candidate.pop(i)
+                    for j in range(i, 18):
+                        if(subset[j]) != -1:
+                            subset[j] -= 1
 
-                candidate = candidate[:18]
+            candidate = candidate[:18]
 
-                for i in range(18):
-                    candidate[i][0] *= 384
-                    candidate[i][1] *= 512
+            for i in range(18):
+                candidate[i][0] *= 384
+                candidate[i][1] *= 512
 
-                keypoints = {"pose_keypoints_2d": candidate}
+            keypoints = {"pose_keypoints_2d": candidate}
+            # with open("/home/aigc/ProjectVTON/OpenPose/keypoints/keypoints.json", "w") as f:
+            #     json.dump(keypoints, f)
+            #
+            # # print(candidate)
+            # output_image = cv2.resize(cv2.cvtColor(detected_map, cv2.COLOR_BGR2RGB), (768, 1024))
+            # cv2.imwrite('/home/aigc/ProjectVTON/OpenPose/keypoints/out_pose.jpg', output_image)
 
         return keypoints
 
 
 if __name__ == '__main__':
-    model = OpenPose(0)
+
+    model = OpenPose()
     model('./images/bad_model.jpg')
