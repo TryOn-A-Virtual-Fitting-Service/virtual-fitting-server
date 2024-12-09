85c53a2ce4f354a4aef30ac0259fd356e3b40c43
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-12-10 02:03:01 +0900

    added time.sleep to check vram manually

--- run/run_ootd.py
+++ run/run_ootd.py
@@ -9,6 +9,8 @@ from preprocess.humanparsing.run_parsing import Parsing
 from ootd.inference_ootd_hd import OOTDiffusionHD
 from ootd.inference_ootd_dc import OOTDiffusionDC
 
+import time
+
 def run_ootd(model_path, cloth_path, accelerator, gpu_id=0, model_type="hd", category=0, scale=2.0, step=40, sample=1, seed=-1):
     import gc
     import torch
@@ -17,13 +19,16 @@ def run_ootd(model_path, cloth_path, accelerator, gpu_id=0, model_type="hd", cat
     
     print(f"Loading model: openpose")
     openpose_model = OpenPose(gpu_id)
+    time.sleep(10)
 
     print(f"Loading model: human parsing")
     parsing_model = Parsing(gpu_id)
+    time.sleep(10)
 
     category_dict = ['upperbody', 'lowerbody', 'dress']
     category_dict_utils = ['upper_body', 'lower_body', 'dresses']
 
+    print(f"Loading model: OOTD")
     if model_type == "hd":
         model = OOTDiffusionHD(gpu_id, accelerator)
     elif model_type == "dc":
@@ -33,23 +38,33 @@ def run_ootd(model_path, cloth_path, accelerator, gpu_id=0, model_type="hd", cat
 
     if model_type == 'hd' and category != 0:
         raise ValueError("model_type 'hd' requires category == 0 (upperbody)!")
+    print(f"Models loaded")
+    time.sleep(10)
 
     print(f"Resizing images")
     cloth_img = Image.open(cloth_path).resize((768, 1024))
     model_img = Image.open(model_path).resize((768, 1024))
-    
+    print(f"Images resized")
+    time.sleep(10)
+
     print(f"Running openpose and human parsing")
     keypoints = openpose_model(model_img.resize((384, 512)))
     model_parse, _ = parsing_model(model_img.resize((384, 512)))
+    print(f"Openpose and human parsing finished")
+    time.sleep(10)
 
     print(f"Getting mask location")
     mask, mask_gray = get_mask_location(model_type, category_dict_utils[category], model_parse, keypoints)
     mask = mask.resize((768, 1024), Image.NEAREST)
     mask_gray = mask_gray.resize((768, 1024), Image.NEAREST)
+    print(f"Mask location obtained")
+    time.sleep(10)
 
     print(f"Creating masked image")
     masked_vton_img = Image.composite(mask_gray, model_img, mask)
     # masked_vton_img.save('./results/mask.jpg')
+    print(f"Masked image created")
+    time.sleep(10)
 
     print(f"Running OOTD model")
     images = model(
@@ -64,6 +79,9 @@ def run_ootd(model_path, cloth_path, accelerator, gpu_id=0, model_type="hd", cat
         image_scale=scale,
         seed=seed,
     )
+    print(f"OOTD model finished")
+    time.sleep(10)
+
     image = None
     if type(images) == list:
         image = images[0]
