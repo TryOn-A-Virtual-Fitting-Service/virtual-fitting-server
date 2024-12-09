3f43f39e0b7b525bc8def37436beb5ce02c6e696
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-12-10 02:30:34 +0900

    Update inference_ootd_hd.py

--- ootd/inference_ootd_hd.py
+++ ootd/inference_ootd_hd.py
@@ -1,32 +1,24 @@
 import pdb
 from pathlib import Path
 import sys
-PROJECT_ROOT = Path(__file__).absolute().parents[0].absolute()
-sys.path.insert(0, str(PROJECT_ROOT))
 import os
 import torch
 import numpy as np
 from PIL import Image
 import cv2
 import gc
-import torch
 import random
 import time
-import pdb
+import threading
 
 from accelerate import Accelerator
-
 from pipelines_ootd.pipeline_ootd import OotdPipeline
 from pipelines_ootd.unet_garm_2d_condition import UNetGarm2DConditionModel
 from pipelines_ootd.unet_vton_2d_condition import UNetVton2DConditionModel
-from diffusers import UniPCMultistepScheduler
-from diffusers import AutoencoderKL
-
+from diffusers import UniPCMultistepScheduler, AutoencoderKL
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import AutoProcessor, CLIPVisionModelWithProjection
-from transformers import CLIPTextModel, CLIPTokenizer
-import threading
+from transformers import AutoProcessor, CLIPVisionModelWithProjection, CLIPTextModel, CLIPTokenizer
 
 VIT_PATH = "./checkpoints/clip-vit-large-patch14"
 VAE_PATH = "./checkpoints/ootd"
@@ -34,14 +26,25 @@ UNET_PATH = "./checkpoints/ootd/ootd_hd/checkpoint-36000"
 MODEL_PATH = "./checkpoints/ootd"
 
 class OOTDiffusionHD:
+    _instance = None
+
+    def __new__(cls, gpu_id, accelerator):
+        if cls._instance is None:
+            cls._instance = super(OOTDiffusionHD, cls).__new__(cls)
+            cls._instance.__initialized = False
+        return cls._instance
 
     def __init__(self, gpu_id, accelerator):
+        if self.__initialized:
+            return
+        self.__initialized = True
 
         self.accelerator = accelerator
         self.gpu_id = self.accelerator.device
-        # self.accelerator = Accelerator(mixed_precision='fp16')
 
         print("Current working directory:", os.getcwd())
+
+        # Load models
         vae = AutoencoderKL.from_pretrained(
             VAE_PATH,
             subfolder="vae",
@@ -60,10 +63,7 @@ class OOTDiffusionHD:
             torch_dtype=torch.float16,
             use_safetensors=True,
         )
-        
-        unet_vton.enable_xformers_memory_efficient_attention()
-        unet_garm.enable_xformers_memory_efficient_attention()
-        
+
         vae, unet_garm, unet_vton = self.accelerator.prepare(vae, unet_garm, unet_vton)
 
         self.pipe = OotdPipeline.from_pretrained(
@@ -72,14 +72,13 @@ class OOTDiffusionHD:
             unet_vton=unet_vton,
             vae=vae,
             torch_dtype=torch.float16,
-            # variant="fp16",
             use_safetensors=True,
             safety_checker=None,
             requires_safety_checker=False,
         ).to(self.accelerator.device)
 
         self.pipe.scheduler = UniPCMultistepScheduler.from_config(self.pipe.scheduler.config)
-        
+
         self.auto_processor = AutoProcessor.from_pretrained(VIT_PATH)
         self.image_encoder = CLIPVisionModelWithProjection.from_pretrained(VIT_PATH)
 
@@ -92,38 +91,29 @@ class OOTDiffusionHD:
             subfolder="text_encoder",
         )
 
-
     def tokenize_captions(self, captions, max_length):
         inputs = self.tokenizer(
             captions, max_length=max_length, padding="max_length", truncation=True, return_tensors="pt"
         )
         return inputs.input_ids
 
-    def cleanup(self):
-        del self.pipe
-        del self.auto_processor
-        del self.image_encoder
-        del self.tokenizer
-        del self.text_encoder
-        gc.collect()
-        torch.cuda.empty_cache()
-
-    def __call__(self,
-                model_type='hd',
-                category='upperbody',
-                image_garm=None,
-                image_vton=None,
-                mask=None,
-                image_ori=None,
-                num_samples=1,
-                num_steps=20,
-                image_scale=1.0,
-                seed=-1,
+    def __call__(
+        self,
+        model_type='hd',
+        category='upperbody',
+        image_garm=None,
+        image_vton=None,
+        mask=None,
+        image_ori=None,
+        num_samples=1,
+        num_steps=20,
+        image_scale=1.0,
+        seed=-1,
     ):
         if seed == -1:
             random.seed(time.time())
             seed = random.randint(0, 2147483647)
-        print('Initial seed: ' + str(seed))
+        print('Initial seed:', seed)
         generator = torch.manual_seed(seed)
 
         with torch.no_grad():
@@ -137,18 +127,35 @@ class OOTDiffusionHD:
                 prompt_embeds = self.text_encoder(self.tokenize_captions([category], 3))[0]
                 prompt_embeds = torch.cat([prompt_embeds, prompt_image], dim=1)
             else:
-                raise ValueError("model_type must be \'hd\' or \'dc\'!")
+                raise ValueError("model_type must be 'hd' or 'dc'!")
 
-            images = self.pipe(prompt_embeds=prompt_embeds,
-                        image_garm=image_garm,
-                        image_vton=image_vton, 
-                        mask=mask,
-                        image_ori=image_ori,
-                        num_inference_steps=num_steps,
-                        image_guidance_scale=image_scale,
-                        num_images_per_prompt=num_samples,
-                        generator=generator,
+            images = self.pipe(
+                prompt_embeds=prompt_embeds,
+                image_garm=image_garm,
+                image_vton=image_vton,
+                mask=mask,
+                image_ori=image_ori,
+                num_inference_steps=num_steps,
+                image_guidance_scale=image_scale,
+                num_images_per_prompt=num_samples,
+                generator=generator,
             ).images
-        
+
+        def cleanup():
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        cleanup_thread = threading.Thread(target=cleanup)
+        cleanup_thread.start()
+
         return images
-    
+
+    def free_memory(self):
+        del self.pipe
+        del self.image_encoder
+        del self.text_encoder
+        del self.auto_processor
+        torch.cuda.empty_cache()
+
+    def __del__(self):
+        self.free_memory()
