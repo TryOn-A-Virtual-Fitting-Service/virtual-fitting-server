8c56192c90ddb8f28c158ef32d2bd29eaedce0a9
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-12-13 11:12:39 +0900

    시드 고정

--- inference/views.py
+++ inference/views.py
@@ -210,7 +210,7 @@ def generate(request):
             'data': "Category must be 0 (upper) or 1 (lower)",
         }, status=400)
 
-    image = run_ootd(model_path, clothing_path, accelerator, model_type=model_type, category=category, scale=scale, step=steps)
+    image = run_ootd(model_path, clothing_path, accelerator, model_type=model_type, category=category, scale=scale, step=steps, seed=42)
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         future_clear_cache = executor.submit(clear_cuda_cache)
